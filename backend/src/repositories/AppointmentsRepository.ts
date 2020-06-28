8516977fd71d198cfb68e8e753790f4a3ff1ea1e
Diego Ferreira <diegosf1705@gmail.com>
2020-06-27 21:15:58 -0300

    Applying the concept of DTO

--- backend/src/repositories/AppointmentsRepository.ts
+++ backend/src/repositories/AppointmentsRepository.ts
@@ -1,6 +1,11 @@
 import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointment';
 
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
 class AppointmentsRepository {
   private appointments: Appointment[];
 
@@ -33,11 +38,10 @@ class AppointmentsRepository {
 
   /**
    * Creates a new Appointment and returns it.
-   * @param provider string with the name for the provider.
-   * @param date appointment schedule.
+   * @param CreateAppointmentDTO
    */
-  public create(provider: string, date: Date): Appointment {
-    const appointment = new Appointment(provider, date);
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
 
     this.appointments.push(appointment);
 
