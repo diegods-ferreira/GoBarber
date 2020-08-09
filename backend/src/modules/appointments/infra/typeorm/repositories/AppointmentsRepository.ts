cdc3a76167b191df2793ba91cccf07eec4bedbcf
Diego Ferreira <diegosf1705@gmail.com>
2020-08-09 14:52:00 -0300

    Rewriting Appointments repository

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,22 +1,43 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import Appointment from '../entities/Appointment';
 
-@EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment>
-  implements IAppointmentsRepository {
+class AppointmentsRepository implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   /**
    * Finds an Appointment by the date passed as a parameter.
    * If found, returns the Appointment, otherwise null.
    * @param date key for search
    */
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
     return findAppointment;
+  }
+
+  /**
+   * Creates an Appointment instance with the data passed as parameter,
+   * saves it in the database and, finally, returns it.
+   * @param ICreateAppointmentDTO
+   */
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
   }
 }
 
