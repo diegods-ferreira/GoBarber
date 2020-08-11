192e0d061f90813c29878531582bbd78028ce2b7
Diego Ferreira <diegosf1705@gmail.com>
2020-08-11 19:38:13 -0300

    Dependency Inversion for HashProvider

--- backend/src/modules/users/services/AuthenticateUserService.ts
+++ backend/src/modules/users/services/AuthenticateUserService.ts
@@ -4,6 +4,7 @@ import { injectable, inject } from 'tsyringe';
 import authConfig from '@config/auth';
 
 import AppError from '@shared/errors/AppError';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
@@ -21,8 +22,8 @@ interface IResponse {
 @injectable()
 class AuthenticateUserSerice {
   constructor(
-    @inject('UsersRepository')
-    private usersRepository: IUsersRepository,
+    @inject('UsersRepository') private usersRepository: IUsersRepository,
+    @inject('HashProvider') private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -32,7 +33,10 @@ class AuthenticateUserSerice {
       throw new AppError('Incorrect email/password combination', 401);
     }
 
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
 
     if (!passwordMatched) {
       throw new AppError('Incorrect email/password combination', 401);
