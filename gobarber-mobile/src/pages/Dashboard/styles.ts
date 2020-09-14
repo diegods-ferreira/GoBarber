c551a5f1af77a2a9320aa85005b76c9017a38c4c
Diego Ferreira <diegosf1705@gmail.com>
2020-09-14 09:40:55 -0300

    Searching providers from the API

--- gobarber-mobile/src/pages/Dashboard/styles.ts
+++ gobarber-mobile/src/pages/Dashboard/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { FlatList } from 'react-native';
+
+import { Provider } from './index';
 
 export const Container = styled.View``;
 
@@ -32,3 +35,5 @@ export const UserAvatar = styled.Image`
   height: 56px;
   border-radius: 50px;
 `;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)``;
