cdeddba254156c59b4b8b73c8e24ff6f79fcd56f
Diego Ferreira <diegosf1705@gmail.com>
2020-09-14 10:23:41 -0300

    Listing providers

--- gobarber-mobile/src/pages/CreateAppointment/index.tsx
+++ gobarber-mobile/src/pages/CreateAppointment/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { View } from 'react-native';
 
 // import { Container } from './styles';
 
 const CreateAppointment: React.FC = () => {
-  return <div />;
+  return <View />;
 };
 
 export default CreateAppointment;
